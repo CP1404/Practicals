4e20ef4ffbd21f48267d88a3e34604e3ca2d2f54
Lindsay Ward <lindsay.ward@jcu.edu.au>
2016-07-08 22:13:25 +1000

    Add brokenScore program to be fixed

---
+++ Prac01/brokenScore.py
@@ -0,0 +1,19 @@
+"""
+CP1404/CP5632 - Practical 01
+Broken program to determine score status
+"""
+
+# TODO: Fix this!
+
+score = float(input("Enter score: "))
+if score < 0:
+    print("Invalid score")
+else:
+    if score > 100:
+        print("Invalid score")
+    if score > 50:
+        print("Passable")
+    if score > 90:
+    print("Excellent")
+if score < 50:
+    print("Bad")
--- Prac01/temperatures.py
+++ Prac01/temperatures.py
@@ -1,5 +1,5 @@
 """
-CP1404 - Practical 01
+CP1404/CP5632 - Practical 01
 Pseudocode for temperature conversion
 """
 __author__ = 'Lindsay Ward'
