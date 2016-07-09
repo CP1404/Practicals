c0cfe04f891a6db62ef0e3157387d15363335094
Lindsay Ward <lindsay.ward@jcu.edu.au>
2016-07-09 21:19:56 +1000

    Add prac 4 files

---
+++ Prac04/scores.py
@@ -0,0 +1,32 @@
+"""
+CP1404/CP5632 Practical
+CSV scores file program - broken one
+scores file stores scores for each subject for 10 people.
+This code reads the lines into lists, saves the first line as a list of subject codes and
+converts the rest of the lines from a list of strings into a list of numbers,
+which it then prints with the maximum value for that subject.
+Nice. Except, it’s broken! It reads the lists per user not per subject so the results are incorrect.
+Use the debugger to follow what it's doing... then fix it.
+"""
+
+
+def main():
+    scores_file = open("scores.csv")
+    scores_data = scores_file.readlines()
+    print(scores_data)
+    subjects = scores_data[0].strip().split(",")
+    score_values = []
+    for score_line in scores_data[1:]:
+        score_strings = score_line.strip().split(",")
+        score_numbers = [int(value) for value in score_strings]
+        score_values.append(score_numbers)
+    scores_file.close()
+    for i in range(len(subjects)):
+        print(subjects[i], "Scores:")
+        for score in score_values[i]:
+            print(score)
+        print("Max:", max(score_values[i]))
+        print()
+
+
+main()
---
+++ Prac04/totalIncome.py
@@ -0,0 +1,23 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+def main():
+    incomes = []
+    months = int(input("How many months? "))
+
+    for month in range(1, months + 1):
+        income = float(input("Enter income for month " + str(month) + ": "))
+        incomes.append(income)
+
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, months + 1):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
+
+main()
