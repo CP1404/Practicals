27d23bd4c1b61fb79b8d10c27651642544d735e2
Lindsay Ward <lindsay.ward@jcu.edu.au>
2016-07-09 14:38:15 +1000

    Add word generator for prac 2

---
+++ Prac02/wordGenerator.py
@@ -0,0 +1,20 @@
+"""
+CP1404/CP5632 - Practical
+Random word generator - based on format of words
+
+(Another way to get just consonants would be to use string.ascii_lowercase (all letters) and remove the vowels)
+"""
+import random
+
+VOWELS = "aeiou"
+CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+
+word_format = "ccvcvvc"
+word = ""
+for kind in word_format:
+    if kind == "c":
+        word += random.choice(CONSONANTS)
+    else:
+        word += random.choice(VOWELS)
+
+print(word)
