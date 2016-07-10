69e2e7804709c98093b461f5681c0b9c94eca38e
Lindsay Ward <lindsay.ward@jcu.edu.au>
2016-07-10 20:31:07 +1000

    Add prac 8 Kivy squaring program

---
+++ Prac06/usedCars.py
@@ -0,0 +1,19 @@
+"""
+CP1404/CP5632 Practical
+Client code to use the Car class
+Note that the import has a folder (module) in it.
+"""
+from Prac06.car import Car
+
+
+def main():
+    bus = Car(180)
+    bus.drive(30)
+    print("fuel =", bus.fuel)
+    print("odo =", bus.odometer)
+    print(bus)
+
+    print("Car {}, {}".format(bus.fuel, bus.odometer))
+    print("Car {self.fuel}, {self.odometer}".format(self=bus))
+
+main()
---
+++ Prac08/squaring.py
@@ -0,0 +1,31 @@
+"""
+CP1404/CP5632 Practical
+Kivy GUI program to square a number
+Lindsay Ward, IT@JCU
+Started 13/10/2015
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+__author__ = 'Lindsay Ward'
+
+
+class SquareNumberApp(App):
+    """ SquareNumberApp is a Kivy App for squaring a number """
+    def build(self):
+        """ build the Kivy app from the kv file """
+        Window.size = (200, 100)
+        self.title = "Square Number"
+        self.root = Builder.load_file('squaring.kv')
+        return self.root
+
+    def handle_calculate(self):
+        """ handle calculation (could be button press or other call), output result to label widget """
+        value = float(self.root.ids.input_number.text)
+        result = value ** 2
+        self.root.ids.output_label.text = str(result)
+
+
+SquareNumberApp().run()
