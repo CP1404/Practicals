b991f0fdab3561092de7632e44769fd17ecdf98d
Lindsay Ward <lindsay.ward@jcu.edu.au>
2016-07-19 16:07:58 +1000

    Add prac 6 sorting with itemgetter

--- Prac05/stateNames.py
+++ Prac05/stateNames.py
@@ -4,7 +4,7 @@ State names in a dictionary
 File needs reformatting
 """
 
-
+# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 STATE_NAMES = {"QLD":"Queensland", "NSW": "New South Wales", "NT" :"Northern Territory", "WA" : "Western Australia", "ACT":"Australian Capital Territory", "VIC":"Victoria", "TAS":"Tasmania"}
 # print(STATE_NAMES)
 
@@ -15,3 +15,6 @@ while state != "":
     else:
         print("Invalid short state")
     state = input("Enter short state: ")
+
+for state in STATE_NAMES:
+    print("{:4} is {}".format(state, STATE_NAMES[state]))
