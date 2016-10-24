499ec47e751c3975849d54452923ca6ecc7346df
Lindsay Ward <lindsay.ward@jcu.edu.au>
2016-10-24 11:29:23 +1000

    Update testing with clarification and hints

--- Prac12/testing.py
+++ Prac12/testing.py
@@ -34,15 +34,17 @@ def run_tests():
     assert repeat_string("hi", 2) == "hi hi"
 
     # TODO: 1. fix the repeat_string function above so that it passes the test
-    # Hint: join
+    # Hint: "-".join(["yo", "yo"] -> "yo-yo"
 
     # assert test with custom message - used to see if Car's init method sets the odometer correctly
     # this should pass (no output)
     test_car = Car()
     assert test_car.odometer == 0, "Car does not set odometer correctly"
 
-    test_car = Car(10)
     # TODO: 2. write assert statements to show if Car sets the fuel correctly
+    # Note that Car's __init__ function sets the fuel in one of two ways: using the value passed in or the default
+    # You should test both of these
+    test_car = Car(fuel=10)
 
 
 run_tests()
@@ -50,12 +52,12 @@ run_tests()
 # TODO: 3. Uncomment the following line and run the doctests
 # doctest.testmod()
 
-# TODO: 4. Fix the failing is_long_word function
+# TODO: 4. Fix the failing is_long_word function (don't change the tests, but the function!)
 
 # TODO: 5. Write and test a function to format a phrase as a sentence - starting with a capital and ending with a single full stop
 # Important: start with a function header and just use pass as the body
 # then add doctests so that:
-# "hello" -> "Hello."
-# "It is an ex parrot." -> "It is an ex parrot."
+# 'hello' -> ''Hello.'
+# 'It is an ex parrot.' -> 'It is an ex parrot.'
 # and one more you decide (that's valid!)
 # then write the body of the function so that the tests pass
