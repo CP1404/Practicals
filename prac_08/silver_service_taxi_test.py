19483afc45ac08796407971a1eccbb24c3ac4c79
Lindsay Ward <lindsay.ward@jcu.edu.au>
2017-07-26 11:35:37 +1000

    Add rounding to Taxi in prac 08 solution

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -7,7 +7,7 @@ from prac_08.silver_service_taxi import SilverServiceTaxi
 
 
 def main():
-    taxi = SilverServiceTaxi("Test Fancy Taxi", 100, 4)
+    taxi = SilverServiceTaxi("Test Fancy Taxi", 100, 2)
     taxi.drive(18)
     print(taxi)
     print(taxi.get_fare())
--- prac_08/taxi.py
+++ prac_08/taxi.py
@@ -24,7 +24,9 @@ class Taxi(Car):
 
     def get_fare(self):
         """Return the price for the taxi trip."""
-        return self.price_per_km * self.current_fare_distance
+        # round fare result to nearest 10c
+        fare = round(self.price_per_km * self.current_fare_distance, 1)
+        return fare
 
     def start_fare(self):
         """Begin a new fare."""
