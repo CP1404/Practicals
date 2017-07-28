e37c70820c41b2b86082eb0d02a2627d133188b9
Lindsay Ward <lindsay.ward@jcu.edu.au>
2017-07-28 14:39:29 +1000

    Add car as separate file and remove from taxi, prac_08

--- prac_08/car.py
+++ prac_08/car.py
@@ -1,16 +1,26 @@
-"""CP1404/CP5632 Practical - Car class example."""
+"""
+CP1404/CP5632 Practical
+Car class
+"""
 
 
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
+        name: string, reference name for car
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
         self.odometer = 0
+
+    def __str__(self):
+        """Return a string representation of a Car object."""
+        return "{}, fuel={}, odometer={}".format(self.name, self.fuel,
+                                                 self.odometer)
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
--- prac_08/taxi.py
+++ prac_08/taxi.py
@@ -2,38 +2,7 @@
 CP1404/CP5632 Practical
 Car class
 """
-
-
-class Car:
-    """Represent a Car object."""
-
-    def __init__(self, name="", fuel=0):
-        """Initialise a Car instance."""
-        self.name = name
-        self.fuel = fuel
-        self.odometer = 0
-
-    def __str__(self):
-        """Return string representation of a Car."""
-        return "{}, fuel={}, odo={}".format(self.name, self.fuel,
-                                            self.odometer)
-
-    def add_fuel(self, amount):
-        """Add amount to the car's fuel."""
-        self.fuel += amount
-
-    def drive(self, distance):
-        """Drive the car a given distance if it has enough fuel,
-        or drive until fuel runs out.
-        Return the distance actually driven."""
-        if distance > self.fuel:
-            distance_driven = self.fuel
-            self.fuel = 0
-        else:
-            self.fuel -= distance
-            distance_driven = distance
-        self.odometer += distance_driven
-        return distance_driven
+from prac_08.car import Car
 
 
 class Taxi(Car):
