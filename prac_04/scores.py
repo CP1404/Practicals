b5f115d00dab9760318a083a1c7c9f5a251434b6
Lindsay Ward <lindsay.ward@jcu.edu.au>
2017-08-24 09:45:11 +1000

    Update function parameters, docstrings, naming and string formatting

--- prac_04/scores.py
+++ prac_04/scores.py
@@ -1,11 +1,16 @@
 """
-CP1404/CP5632 Practical
+CP1404/CP5632 Practical - Suggested Solution
+This solution was submitted (via GitHub PR) by 2017-2 student CraigMorris1986!
+Note the effective use of the pop(0) list method to both use and remove the
+first score in each list (moving it to a new list for each subject).
+
 Debugging exercise: almost-working version of a CSV scores file program.
 The scores.csv file stores scores for each subject for 10 people.
-This code reads the lines into lists, saves the first line as a list of subject codes and
-converts the rest of the lines from a list of strings into a list of numbers,
-which it then prints with the maximum value for that subject.
-Nice. Except, it’s broken! It reads the lists per user not per subject so the results are incorrect.
+This code reads the lines into lists, saves the first line as a list of subject
+codes and converts the rest of the lines from a list of strings into a list of
+numbers, which it then prints with the maximum value for that subject.
+Nice. Except, it’s broken! It reads the lists per user not per subject so the
+results are incorrect.
 Use the debugger to follow what it's doing... then fix it.
 """
 
@@ -14,7 +19,7 @@ def main():
     """Read and display student scores from scores file."""
     scores_file = open("scores.csv")
     scores_data = scores_file.readlines()
-    print(scores_data)
+    # print(scores_data)
     subjects = scores_data[0].strip().split(",")
     score_values = []
     for score_line in scores_data[1:]:
@@ -22,30 +27,33 @@ def main():
         score_numbers = [int(value) for value in score_strings]
         score_values.append(score_numbers)
     scores_file.close()
-    scores_by_subject = organise_by_index(score_values, len(subjects))
-    display_subject_details(scores_by_subject, subjects, len(subjects))
+    scores_by_subject = reorganise_scores_by_subject(score_values)
+    display_subject_details(scores_by_subject, subjects)
 
 
-def organise_by_index(score_values, number_of_subjects):
-    """This function takes in the list of scores values and number of subjects and pops the first index of each line
-    of scores into a new list and returns that list. """
+def reorganise_scores_by_subject(score_values):
+    """Process list of by-student scores into list of by-subject scores."""
     subject_scores = []
+    number_of_subjects = len(score_values[0])
     for i in range(number_of_subjects):
-        temp_list = []
+        scores_for_one_subject = []
         for scores in score_values:
-            temp_list.append(scores.pop(0))
-        subject_scores.append(temp_list)
+            # pop first score from by-student scores into the by-subject scores
+            scores_for_one_subject.append(scores.pop(0))
+        subject_scores.append(scores_for_one_subject)
     return subject_scores
 
 
-def display_subject_details(scores_by_subject, subject_names, number_of_subjects):
-    """This function takes in the list of scores by subject code, the subject names list and the number of subjects
-    to display formatted output to user. Print statements include the MAX, MIN, and Average calculations"""
-    for i in range(number_of_subjects):
+def display_subject_details(scores_by_subject, subject_names):
+    """Display subject scores with statistics."""
+    for i, scores_for_one_subject in enumerate(scores_by_subject):
         print(subject_names[i], "Scores:")
-        for score in scores_by_subject[i]:
+        for score in scores_for_one_subject:
             print("{:>2}".format(score))
-        print("Max: ", max(scores_by_subject[i]))
-        print("Min: ", min(scores_by_subject[i]))
-        print("Avg: {:.2f}".format((int(sum(scores_by_subject[i]) / len(scores_by_subject[i])))), "\n")
+        print("Max: {:3}".format(max(scores_for_one_subject)))
+        print("Min: {:3}".format(min(scores_for_one_subject)))
+        print("Avg: {:6.2f}\n".format(
+            (sum(scores_for_one_subject) / len(scores_for_one_subject))))
+
+
 main()
