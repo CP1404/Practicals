cf1d118fa99a0dd2de8442f812862af4b5bac4db
Lindsay Ward <lindsay.ward@jcu.edu.au>
2017-10-17 09:33:37 +1000

    Add list_comprehension solution to prac_04

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -13,22 +13,32 @@ for name in names:
     first_initials.append(name[0])
 print(first_initials)
 
-# list comprehension that does the same thing
+# list comprehension that does the same thing as the loop above
 first_initials = [name[0] for name in names]
 print(first_initials)
 
-# list comprehension that creates a list containing tuples of both initials
-# splits each name and adds the first letter of each part to a tuple
-full_initials = [(name.split()[0][0], name.split()[1][0]) for name in
+# list comprehension that creates a list containing the initials
+# splits each name and adds the first letters of each part to a string
+full_initials = [name.split()[0][0] + name.split()[1][0] for name in
                  full_names]
 print(full_initials)
 
-almost_numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-# TODO: use a list comprehension to create a list of integers
-# from this list of strings
-# numbers =
+# one more example, using filtering to select only the names that start with A
+a_names = [name for name in names if name.startswith('A')]
+print(a_names)
 
 # TODO: use a list comprehension to create a list of all of the full_names
 # in lowercase format
-# lowercase_full_names =
+lowercase_full_names = [name.lower() for name in full_names]
+print(lowercase_full_names)
+
+almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
+# TODO: use a list comprehension to create a list of integers
+# from the above list of strings
+numbers = [int(almost_number) for almost_number in almost_numbers]
+print(numbers)
+
+# TODO: use a list comprehension to create a list of only the numbers that are
+# greater than 9 from the numbers (not strings) you just created
+big_numbers = [number for number in numbers if number > 9]
+print(big_numbers)
