eee1793c5fe147150a87a8a1e675c7969066c084
Lindsay Ward <lindsay.ward@jcu.edu.au>
2018-05-09 10:29:31 +1000

    Replace prac 09 cleanup_files with demo walkthrough, update Lyrics

---
+++ prac_09/os_demos.py
@@ -0,0 +1,63 @@
+"""
+CP1404/CP5632 Practical
+Demos of various os module examples
+"""
+import shutil
+import os
+
+
+def main():
+    """Demo of os module functions."""
+    print("Starting directory is: {}".format(os.getcwd()))
+
+    # Change to desired directory
+    os.chdir('Lyrics/Christmas')
+
+    # Print a list of all files in current directory
+    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
+
+    # Make a new directory
+    # The next time you run this, it will crash if the directory exists
+    # TODO: Use exception handling to avoid the crash (just pass)
+    os.mkdir('temp')
+
+    # Loop through each file in the (current) directory
+    for filename in os.listdir('.'):
+        # Ignore directories, just process files
+        if os.path.isdir(filename):
+            continue
+
+        new_name = get_fixed_filename(filename)
+        # print("Renaming {} to {}".format(filename, new_name))
+
+        # TODO: Try these options one at a time
+        # Option 1: rename file to new name - in place
+        # os.rename(filename, new_name)
+
+        # Option 2: move file to new place, with new name
+        # shutil.move(filename, 'temp/' + new_name)
+
+    # Process all subdirectories using os.walk()
+    os.chdir('..')  # '..' means to go 'up' one directory
+    lyrics_path = os.getcwd()  # store the path so we can get back to it
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
+
+        # TODO: change into the directory and print the current working directory
+        # then change back to the lyrics_path
+        # Note: if you get this wrong, walk will stop short,
+        # so you need to check it still walks through all subdirectories
+
+        # TODO: add a loop (in between directory changes) to rename the files
+
+
+def get_fixed_filename(filename):
+    """Return a 'fixed' version of filename."""
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    return new_name
+
+
+main()
