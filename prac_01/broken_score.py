68fdff7ac9cd5b0c56dd1db348263ff9767f40e3
Lindsay Ward <lindsay.ward@jcu.edu.au>
2018-08-15 11:24:49 +1000

    Add comments to broken score including Zen of Python reference

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -3,8 +3,15 @@ CP1404/CP5632 - Practical - Suggested Solution
 Fixed program to determine score status
 """
 
-# Note boundary conditions (50 should be a pass, not > 50)
-# Note efficiency and nesting; use the fewest number of if/elif as possible
+# Check your boundary conditions (e.g. >= 50 should be a pass, not just > 50)
+# Think about efficiency and nesting; use the fewest number of if/elif
+# "The Zen of Python" says, "Flat is better than nested."
+# https://www.python.org/dev/peps/pep-0020/
+# Also note that here we only have one path for "Invalid score" (DRY principle)
+# You want to make sure the last path is "else", not "elif" as we always want a result here,
+# so there should be no final condition (if it wasn't one of the earlier possibilities,
+# it must be the last thing, no need to check if it is)
+
 
 score = float(input("Enter score: "))
 if score < 0 or score > 100:
