befa525767e951875577e4b113f34798a9aab36c
Lindsay Ward <lindsay.ward@jcu.edu.au>
2018-09-06 11:41:56 +1000

    Add languages solution to prac 06

---
+++ prac_06/languages.py
@@ -0,0 +1,20 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+Programming Language client code.
+"""
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [ruby, python, visual_basic]
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
