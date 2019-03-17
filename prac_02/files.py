abe72a1e59a885a4f76936196286d25669ff2291
Lindsay Ward <lindsay.ward@jcu.edu.au>
2019-03-18 07:56:02 +1000

    Move close() for files in prac 2 to earliest spot

--- prac_02/files.py
+++ prac_02/files.py
@@ -6,22 +6,22 @@ Quick file opening/writing/reading exercises
 # Quick Program 1
 out_file = open("name.txt", "w")
 name = input("What is your name? ")
-print(name, file=out_file)  # or outFile.write(name)
+print(name, file=out_file)  # or out_file.write(name)
 out_file.close()
 
 # Quick Program 2
 in_file = open("name.txt", "r")
 name = in_file.read().strip()
-print("Your name is", name)
 in_file.close()
+print("Your name is", name)
 
 # Quick Program 3
 # Note that .strip() is unnecessary since int() handles that whitespace
 in_file = open("numbers.txt", "r")
 number1 = int(in_file.readline())
 number2 = int(in_file.readline())
-print(number1 + number2)
 in_file.close()
+print(number1 + number2)
 
 # Quick Program 3 extended - sum of all numbers
 in_file = open("numbers.txt", "r")
@@ -29,5 +29,5 @@ total = 0
 for line in in_file:
     number = int(line)
     total += number
-print(total)
 in_file.close()
+print(total)
