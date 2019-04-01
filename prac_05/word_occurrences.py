2396d4a4d2d84f6f34f398bb81826b64a8a68622
Lindsay Ward <lindsay.ward@jcu.edu.au>
2019-04-02 09:00:14 +1000

    Update prac 5 word occurrences with better names and comments

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,24 +1,26 @@
 """
 CP1404/CP5632 Practical - Suggested Solution
 Count word occurrences in a string
+This example is in the lecture notes 3 different ways
 """
 
-unique_words = {}
+word_to_count = {}
 text = input("Text: ")
 # text = "this is a collection of words of nice words this is a fun thing it is"
 words = text.split()
 for word in words:
-    frequency = unique_words.get(word, 0)
     # Note: this is the "Look Before You Leap" (LBYL) pattern
-    # we could use the "Easier to Ask Forgiveness" pattern using exceptions
-    unique_words[word] = frequency + 1
+    # we could use the "Easier to Ask Forgiveness" (EAFP) pattern using exceptions
+    frequency = word_to_count.get(word, 0)
+    word_to_count[word] = frequency + 1
 
 # Print the unique words and their frequencies,
 # in alphabetical order
-words = list(unique_words.keys())
+words = list(word_to_count.keys())
 words.sort()
+
 # use the max function to find the maximum of the values produced by the
 # generator function (like a list comprehension) of lengths of words
 max_length = max((len(word) for word in words))
 for word in words:
-    print("{:{}} : {}".format(word, max_length, unique_words[word]))
+    print("{:{}} : {}".format(word, max_length, word_to_count[word]))
