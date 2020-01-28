2c35011d12738d17029d33fb8e6df5ca15d906f8
Lindsay Ward <lindsay.ward@jcu.edu.au>
2020-01-28 17:42:26 +1000

    Add prac 5 email to name solution

---
+++ prac_05/emails.py
@@ -0,0 +1,31 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+Email to name dictionary
+"""
+
+
+def main():
+    """Create dictionary of emails-to-names."""
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = get_name_from_email(email)
+        confirmation = input("Is your name {}? (Y/n) ".format(name))
+        if confirmation.upper() != "Y" and confirmation != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print("{} ({})".format(name, email))
+
+
+def get_name_from_email(email):
+    """Extract expected name from email address."""
+    prefix = email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()
+    return name
+
+
+main()
