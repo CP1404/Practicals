d93dc0f8eb11e530e6b48e6ac8f91897372eb584
Lindsay Ward <lindsay.ward@jcu.edu.au>
2020-01-29 09:39:30 +1000

    Update Kivy solutions for better separation of concerns and naming

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -2,7 +2,6 @@
 CP1404/CP5632 Practical - Suggested Solution
 GUI program to convert miles to kilometres
 Lindsay Ward, IT@JCU
-06/10/2015, updated 04/10/2018
 Print statements included to see when the functions run
 """
 
@@ -10,42 +9,41 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 
-MILES_TO_KM = 1.60934
+FACTOR_MILES_TO_KM = 1.60934
 
 
 class MilesConverterApp(App):
-    """MilesConverterApp is a Kivy App for converting miles to kilometres."""
+    """Kivy App for converting miles to kilometres."""
     output_km = StringProperty()
 
     def build(self):
         """Build the Kivy app from the kv file."""
         self.title = "Convert Miles to Kilometres"
-        self.root = Builder.load_file('convert_m_km.kv')
+        self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def handle_calculate(self, text):
         """Handle calculation (could be button press or other call)."""
-        print("handle calc")
+        print("handle calculate")
         miles = self.convert_to_number(text)
         self.update_result(miles)
 
     def handle_increment(self, text, change):
         """Handle up/down button press, update the text input with new value, call calculation function."""
-        print("handle inc")
+        print("handle increment")
         miles = self.convert_to_number(text) + change
         self.root.ids.input_miles.text = str(miles)
         # Since the InputText.text has changed, its on_text event will fire and handle_calculate will be called
 
     def update_result(self, miles):
         print("update")
-        self.output_km = str(miles * MILES_TO_KM)
+        self.output_km = str(miles * FACTOR_MILES_TO_KM)
 
     @staticmethod
     def convert_to_number(text):
         """Convert text to float or 0.0 if invalid."""
         try:
-            value = float(text)
-            return value
+            return float(text)
         except ValueError:
             return 0.0
 
--- prac_07/squaring.py
+++ prac_07/squaring.py
@@ -2,7 +2,6 @@
 CP1404/CP5632 Practical
 Kivy GUI program to square a number
 Lindsay Ward, IT@JCU
-Started 13/10/2015
 """
 
 from kivy.app import App
@@ -21,10 +20,12 @@ class SquareNumberApp(App):
         return self.root
 
     def handle_calculate(self, value):
-        """Handle calculation (could be button press or other call),
-        output result to label widget."""
-        result = value ** 2
-        self.root.ids.output_label.text = str(result)
+        """Handle calculation (could be button press or other call), output result to label widget."""
+        try:
+            result = int(value) ** 2
+            self.root.ids.output_label.text = str(result)
+        except ValueError:
+            self.root.ids.output_label.text = ""
 
 
 SquareNumberApp().run()
