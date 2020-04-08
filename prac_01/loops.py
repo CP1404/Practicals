14a98f376a8c0c366dfe762f4d53ca2b3b7304ff
wujiaze0324 <62562869+wujiaze0324@users.noreply.github.com>
2020-04-08 16:53:44 +0800

    Update and rename prac_01/loops.py to  prac_01/loops.py

--- prac_01/loops.py
+++ prac_01/loops.py
@@ -1,1 +1,16 @@
+for i in range(0, 110, 10):
+    print(i, end=' ')
+print()
+
+for i in range(20, 0, -1):
+    print(i, end=' ')
+print()
+
+num = int(input("Enter a number"))
+print(num * "*")
+
+
+num1 = int(input("Enter a number"))
+for i in range(1, num + 1, 1):
+    print('*' * i)
 
