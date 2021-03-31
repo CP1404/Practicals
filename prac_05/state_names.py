45d14da59dde90e42623ef02bc5b70c2e8e6d566
Lindsay Ward <lindsay.ward@jcu.edu.au>
2021-03-31 14:35:42 +1000

    Update prac 5 solution dictionary naming

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,16 +4,15 @@ State names in a dictionary
 File has been reformatted and state inputs can be any case
 """
 
-STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales",
-               "NT": "Northern Territory", "WA": "Western Australia",
-               "ACT": "Australian Capital Territory", "VIC": "Victoria",
-               "TAS": "Tasmania"}
-# print(STATE_NAMES)
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales",
+                "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria",
+                "TAS": "Tasmania"}
 
 state = input("Enter short state: ").upper()
 while state != "":
-    if state in STATE_NAMES:
-        print(state, "is", STATE_NAMES[state])
+    if state in CODE_TO_NAME:
+        print(state, "is", CODE_TO_NAME[state])
     else:
         print("Invalid short state")
     state = input("Enter short state: ").upper()
