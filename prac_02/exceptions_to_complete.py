b7249925ab57f041e158bceca650b6b52a3fe1a6
Lindsay Ward <lindsay.ward@jcu.edu.au>
2021-10-14 16:41:13 +1000

    Remove unnecessary variable definition from exceptions_to_complete

--- prac_02/exceptions_to_complete.py
+++ prac_02/exceptions_to_complete.py
@@ -3,12 +3,11 @@ CP1404/CP5632 - Practical - Suggested Solution
 Fill in the TODOs to complete the task
 """
 
-finished = False
-result = 0
-while not finished:
+is_finished = False
+while not is_finished:
     try:
         result = int(input("Enter a valid integer: "))
-        finished = True
+        is_finished = True
     except ValueError:
         print("Please enter a valid integer.")
 print("Valid result is:", result)
