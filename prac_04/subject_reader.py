d86396c786e90ebc90ac4a9b24a4536d167219a3
Lindsay Ward <lindsay.ward@jcu.edu.au>
2022-10-26 11:27:00 +1000

    Rename data to subjects

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,13 +8,13 @@ FILENAME = "subject_data.txt"
 
 def main():
     """Read subject data and display neatly."""
-    data = get_data()
-    display_subjects(data)
+    subjects = get_subjects()
+    display_subjects(subjects)
 
 
-def get_data():
-    """Read data from file formatted like: subject,lecturer,number of students."""
-    data = []
+def get_subjects():
+    """Read data from file formatted like: code,lecturer,number of students."""
+    subject = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -24,18 +24,18 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
-        data.append(parts)
+        subject.append(parts)
     input_file.close()
-    return data
+    return subject
 
 
-def display_subjects(data):
+def display_subjects(subjects):
     """Display data nicely."""
-    for subject_data in data:
+    for subject in subjects:
         # Print using the format method and *unpacking
-        print("{} is taught by {:12} and has {:3} students".format(*subject_data))
+        print("{} is taught by {:12} and has {:3} students".format(*subject))
         # Another way with f-string instead of format
-        # print(f"{subject_data[0]} is taught by {subject_data[1]:12} and has {subject_data[2]:3} students")
+        # print(f"{subject[0]} is taught by {subject_data[1]:12} and has {subject[2]:3} students")
 
 
 main()
