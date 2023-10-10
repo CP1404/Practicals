0d17dceaa463717ad46538294e872d3390f296af
Lindsay Ward <lindsay.ward@jcu.edu.au>
2023-10-10 10:14:09 +1000

    Reformat state_names.py to match screenshot

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,7 +5,7 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
+CODE_TO_NAME = {"QLD":"Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
             "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
