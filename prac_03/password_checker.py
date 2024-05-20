80dcc8fe6eb30f1f24ad0b25cb8be897466d7a6d
Lindsay Ward <lindsay.ward@jcu.edu.au>
2024-05-20 11:03:04 +1000

    Update prac 3 string formatting and password checker

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -5,26 +5,24 @@ Password checker built from "skeleton" code to help you get started
 
 MIN_LENGTH = 2
 MAX_LENGTH = 6
-SPECIAL_CHARS_REQUIRED = False
+IS_SPECIAL_CHARACTER_REQUIRED = False
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
 def main():
     """Program to get and check a user's password."""
     print("Please enter a valid password")
-    print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH,
-          "characters, and contain:")
+    print(f"Your password must be between {MIN_LENGTH} and {MAX_LENGTH} characters, and contain:")
     print("\t1 or more uppercase characters")
     print("\t1 or more lowercase characters")
     print("\t1 or more numbers")
-    if SPECIAL_CHARS_REQUIRED:
+    if IS_SPECIAL_CHARACTER_REQUIRED:
         print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
     password = input("> ")
     while not is_valid_password(password):
         print("Invalid password!")
         password = input("> ")
-    print("Your " + str(
-        len(password)) + " character password is valid: " + password)
+    print(f"Your {len(password)}-character password is valid: {password}")
 
 
 def is_valid_password(password):
@@ -33,30 +31,28 @@ def is_valid_password(password):
     if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
         return False
 
-    count_lower = 0
-    count_upper = 0
-    count_digit = 0
-    count_special = 0
-    for char in password:
+    number_of_lower = 0
+    number_of_upper = 0
+    number_of_digit = 0
+    number_of_special = 0
+    for character in password:
         # TODO: count each kind of character (use str methods like isdigit)
-        if char.isdigit():
-            count_digit += 1
-        elif char.islower():
-            count_lower += 1
-        elif char.isupper():
-            count_upper += 1
-        elif char in SPECIAL_CHARACTERS:
-            count_special += 1
-
+        if character.isdigit():
+            number_of_digit += 1
+        elif character.islower():
+            number_of_lower += 1
+        elif character.isupper():
+            number_of_upper += 1
+        elif character in SPECIAL_CHARACTERS:
+            number_of_special += 1
+        # Notice the appropriate use of if, elif, no else pattern
     # TODO: if any of the 'normal' counts are zero, return False
-    if count_lower == 0 or count_upper == 0 or count_digit == 0:
+    if number_of_lower == 0 or number_of_upper == 0 or number_of_digit == 0:
         return False
 
-    # TODO: if special characters are required, then check the count of those
-    # and return False if it's zero
-    if SPECIAL_CHARS_REQUIRED:
-        if count_special == 0:
-            return False
+    # TODO: if special characters are required, then check the count of those and return False if it's zero
+    if IS_SPECIAL_CHARACTER_REQUIRED and number_of_special == 0:
+        return False
 
     # if we get here (without returning False), then the password must be valid
     return True
--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -1,47 +1,55 @@
 """
 CP1404/CP5632 - Practical
-Various examples of using Python string formatting with the str.format() method
-Want to read more about it? https://docs.python.org/3/library/string.html#formatstrings
+Various examples of using Python string formatting.
+(We prefer f-strings in this subject.)
+Want to read more about it?
+https://docs.python.org/3/library/string.html#formatstrings
 """
 
 name = "Gibson L-5 CES"
 year = 1922
-cost = 16035.4
+cost = 16035.9
 
-# The ‘old’ manual way to format text with string concatenation:
+# The 'old' manual way to format text with string concatenation (don't do this):
 print("My guitar: " + name + ", first made in " + str(year))
 
-# A better way - using str.format():
+# A better way - using str.format() (don't do this unless you need to):
 print("My guitar: {}, first made in {}".format(name, year))
 print("My guitar: {0}, first made in {1}".format(name, year))
 print("My {0} was first made in {1} (that's right, {1}!)".format(name, year))
 
-# And with f-string formatting (introduced in Python 3.6)
+# And with f-string formatting, introduced in Python 3.6 (do this)
 print(f"My {name} was first made in {year} (that's right, {year}!)")
 
 # Formatting currency (grouping with comma, 2 decimal places):
-print("My {} would cost ${:,.2f}".format(name, cost))
-print(f"My {name} would cost ${cost:,.2f}")
+print("My {} would cost ${:,.2f}".format(name, cost))  # str.format version
+print(f"My {name} would cost ${cost:,.2f}")  # preferred f-string version
 
 # Aligning columns by using width after the :
-# This loop uses enumerate, useful when you want both the index and value
+# This loop uses enumerate, which is useful when you want both the index and value
 numbers = [1, 19, 123, 456, -25]
 
 for i, number in enumerate(numbers, 1):
     print(f"Number {i} is {number:5}")
 
-# TODO: Use string formatting to produce the output:
-# 1922 Gibson L-5 CES for about $16,035!
+# TODO: Use f-string formatting to produce the output:
+# 1922 Gibson L-5 CES for about $16,036!
 
 # Note that the .0f rounds to no decimal places
 print(f"{year} {name} for about ${cost:,.0f}!")
 
-# TODO: Using a for loop with the range function and string formatting,
-# produce the following right-aligned output (do not use a list):
-#   0
-#  50
-# 100
-# 150
+# TODO: Using a for loop with the range function and f-string formatting,
+# produce the following right-aligned output (DO NOT use a list):
+# 2 ^ 0 is    1
+# 2 ^ 1 is    2
+# 2 ^ 2 is    4
+# 2 ^ 3 is    8
+# 2 ^ 4 is   16
+# 2 ^ 5 is   32
+# 2 ^ 6 is   64
+# 2 ^ 7 is  128
+# 2 ^ 8 is  256
+# 2 ^ 9 is  512
+# 2 ^10 is 1024
 
-for number in range(0, 151, 50):
-    print(f"{number:3}")
+# The solution to this question is intentionally not provided
