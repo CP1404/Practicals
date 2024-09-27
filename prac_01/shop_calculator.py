5257260dc32b712ea1b4fcfaaec11295bfce3e89
Lindsay Ward <lindsay.ward@jcu.edu.au>
2024-09-27 11:19:40 +1000

    Improve variable names in shop calculator

--- prac_01/shop_calculator.py
+++ prac_01/shop_calculator.py
@@ -1,21 +1,18 @@
 """
 CP1404/CP5632 - Practical - Suggested Solution
-Shop calculator program to determine total (discounted) price
+Shop calculator program to determine total price with potential discount
 """
 
-total = 0
+total_price = 0
 number_of_items = int(input("Number of items: "))
 while number_of_items < 0:
     print("Invalid number of items!")
     number_of_items = int(input("Number of items: "))
 for i in range(number_of_items):
-    price = float(input("Price of item: "))
-    total += price
+    item_price = float(input("Price of item: "))
+    total_price += item_price
 
-if total > 100:
-    total *= 0.9  # apply 10% discount
+if total_price > 100:
+    total_price *= 0.9  # Apply 10% discount
 
-print("Total price for ", number_of_items, " items is $", total, sep='')
-
-# with f-string formatting for currency output
-print(f"Total price for {number_of_items} items is ${total:.2f}")
+print(f"Total price for {number_of_items} items is ${total_price:.2f}")
