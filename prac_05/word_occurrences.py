d712a4316798d86b869e08347838ac55609bda21
Lindsay Ward <lindsay.ward@jcu.edu.au>
2025-07-11 14:07:59 +1000

    Simplify by sorting dictionary

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,12 +1,12 @@
 """
-CP1404/CP5632 Practical - Suggested Solution
+CP1404 Practical - Suggested Solution
 Count word occurrences in a string
 This example is in the lecture notes 3 different ways
 """
 
 word_to_count = {}
-text = input("Text: ")
-# text = "this is a collection of words of nice words this is a fun thing it is"
+# text = input("Text: ")
+text = "this is a collection of words of nice words this is a fun thing it is"
 words = text.split()
 for word in words:
     # Note: this is the "Look Before You Leap" (LBYL) pattern
@@ -14,13 +14,9 @@ for word in words:
     frequency = word_to_count.get(word, 0)
     word_to_count[word] = frequency + 1
 
-# Print the unique words and their frequencies,
-# in alphabetical order
-words = list(word_to_count.keys())
-words.sort()
-
-# use the max function to find the maximum of the values produced by the
-# generator function (like a list comprehension) of lengths of words
+# Use the max function to find the maximum of the values produced by the
+# generator expression (like a list comprehension) of lengths of words
 max_length = max((len(word) for word in words))
-for word in words:
+# When we sort a dictionary like this we get a list of the keys in sorted order
+for word in sorted(word_to_count):
     print(f"{word:{max_length}} : {word_to_count[word]}")
