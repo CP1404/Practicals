c6afa2905fa4dae4e110aec998f9358853c17525
Lindsay Ward <lindsay.ward@jcu.edu.au>
2025-12-04 17:16:34 +1000

    Rename colours dictionary

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,17 +3,16 @@ CP1404/CP5632 Practical - Suggested Solution
 Hexadecimal colour lookup
 """
 
-COLOUR_CODES = {"aliceblue": "#f0f8ff", "antiquewhite": "#faebd7",
-                "antiquewhite1": "#ffefdb", "antiquewhite2": "#eedfcc",
-                "antiquewhite3": "#cdc0b0", "antiquewhite4": "#8b8378",
-                "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
-                "aquamarine4": "#458b74", "azure1": "#f0ffff",
-                "azure2": "#e0eeee", "azure3": "#c1cdcd", "azure4": "#838b8b",
-                "beige": "#f5f5dc", "bisque1": "#ffe4c4"}
+NAME_TO_HEX = {"aliceblue": "#f0f8ff", "antiquewhite": "#faebd7",
+               "antiquewhite1": "#ffefdb", "antiquewhite2": "#eedfcc",
+               "antiquewhite3": "#cdc0b0", "antiquewhite4": "#8b8378",
+               "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
+               "aquamarine4": "#458b74", "azure1": "#f0ffff",
+               "azure2": "#e0eeee", "azure3": "#c1cdcd", "azure4": "#838b8b",
+               "beige": "#f5f5dc", "bisque1": "#ffe4c4"}
 
 colour_name = input("Enter a colour name: ").lower()
 while colour_name != "":
-    # Note: using the get dictionary method
-    # means you will get None if the key is not found
-    print(f"The code for \"{colour_name}\" is {COLOUR_CODES.get(colour_name)}")
+    # Note: using the get dictionary method means you will get None if the key is not found
+    print(f"The code for \"{colour_name}\" is {NAME_TO_HEX.get(colour_name)}")
     colour_name = input("Enter a colour name: ").lower()
